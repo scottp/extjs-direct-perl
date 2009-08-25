ac5ce6cd5171f481ff097374ebf578c8a672562d
Scott Penrose <scottp@Ermintrude.local>
2009-08-25 10:22:42 +1000

    Include main

--- modperl/startup.pl
+++ modperl/startup.pl
@@ -2,6 +2,6 @@
 
 use lib '/home/ubuntu/4gw/extjs-direct-perl/modperl';
 use lib '/home/ubuntu/4gw/FourGW/api/lib';
-use lib '/home/ubuntu/4gw/FourGW/customer/lib';
+use lib '/home/ubuntu/4gw/FourGW/main/lib';
 
 1;
